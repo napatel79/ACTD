3b9be3194eccbdb338b6e03553ea967da1b48262
NimsTay <ntayefeh@vols.utk.edu>
2023-03-29 14:38:04 -0400

    devices on windows working

--- UI/checkConnection.py
+++ UI/checkConnection.py
@@ -3,8 +3,13 @@ import os
 
 class Connection():
     def __init__(self):
-        self.osType = platform.platform()
+        self.osType = platform.system().lower()
     
     def findConnectedDevices(self):
-        if self.osType.contains('windows'):
-            os.system('Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -match '^USB' }')
+        if 'windows' in self.osType:
+            devices = os.popen('powershell \"Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -match \'^USB\' }\"').read()#basically os.system but saves output to a var
+
+        else:
+            devices = os.popen
+conn = Connection()
+conn.findConnectedDevices()
