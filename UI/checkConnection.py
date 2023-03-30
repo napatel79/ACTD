54f39db2ef679134b0dfa085cc5820f1070e4914
NimsTay <ntayefeh@vols.utk.edu>
2023-03-30 15:47:24 -0400

    not working

--- UI/checkConnection.py
+++ UI/checkConnection.py
@@ -1,5 +1,6 @@
 import platform
 import os
+import time
 
 class Connection():
     def __init__(self):
@@ -8,8 +9,12 @@ class Connection():
     def findConnectedDevices(self):
         if 'windows' in self.osType:
             devices = os.popen('powershell \"Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -match \'^USB\' }\"').read()#basically os.system but saves output to a var
-
+            # if 'nameOfMicroController' in devices: #then the device is plugged in
+            if 1:
+                time.sleep(2)
+                return 1
+            else:
+                return 0
         else:
-            devices = os.popen
-conn = Connection()
-conn.findConnectedDevices()
+            pass
+            #check mac and windows
--- UI/mainUI.py
+++ UI/mainUI.py
@@ -4,13 +4,15 @@ import customtkinter
 import window
 from window import *
 import time
+import checkConnection
+from checkConnection import Connection
 
 class MainWindow(Window):
 
     def __init__(self):
         super().__init__()
         text_var = tkinter.StringVar(value="Please Plug In Your Contact Tracing Device")
-        label = customtkinter.CTkLabel(master=self,
+        self.label = customtkinter.CTkLabel(master=self,
                                textvariable=text_var,
                                width=self.width,
                                height=self.height,
@@ -18,23 +20,28 @@ class MainWindow(Window):
                                bg_color=("black", "gray75"),
                                text_color='black',
                                corner_radius=8)
-        label.configure(font=('Helvatical bold',50))
-        label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.label.configure(font=('Helvatical bold',50))
+        self.label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.button = customtkinter.CTkButton(master=self, corner_radius=30, command=self.update_str, fg_color=("gray75", "blue"))
+        self.button.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
+        self.conn = Connection()
 
     def update_str(self):
         text_var = tkinter.StringVar(value="updated str")
         label = customtkinter.CTkLabel(master=self, textvariable=text_var, width=self.width, height=self.height, fg_color=("black", "gray75"),bg_color=("black", "gray75"),text_color='black',corner_radius=8)
-        label.configure(font=('Helvatical bold',50))
         label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.update()
 
     def wait_for_connection(self):
-        self.after(1000, self.update_str)
+        while self.conn.findConnectedDevices() == 0:
+            pass
+        self.update_str()
 
     def runMainWindow(self):
         self.mainloop()
 
 if __name__ == '__main__':
     ui = MainWindow()
-    ui.wait_for_connection()
+    # ui.wait_for_connection()
     ui.runMainWindow()
     
